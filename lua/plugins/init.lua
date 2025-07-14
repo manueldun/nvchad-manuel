89075836d9cf60675ba928cd7e657e15086626b8
Manuel Fernando Dun Jimenez <manueldun@gmail.com>
2025-07-14 10:35:50 -0400

    Added dap disassembler plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,7 +12,44 @@ return {
       require "configs.lspconfig"
     end,
   },
-  { "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+  { "Jorenar/nvim-dap-disasm", dependencies = { "mfussenegger/nvim-dap", "carriga/nvim-dap-ui" } },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dapDisam = require "dap-disasm"
+      dapDisam.setup {
+        -- Add disassembly view to elements of nvim-dap-ui
+        dapui_register = true,
+
+        -- Add custom REPL commands for stepping with instruction granularity
+        repl_commands = true,
+
+        -- The sign to use for instruction the exectution is stopped at
+        sign = "DapStopped",
+
+        -- Number of instructions to show before the memory reference
+        ins_before_memref = 16,
+
+        -- Number of instructions to show after the memory reference
+        ins_after_memref = 16,
+
+        -- Labels of buttons in winbar
+        controls = {
+          step_into = "Step Into",
+          step_over = "Step Over",
+          step_back = "Step Back",
+        },
+
+        -- Columns to display in the disassembly view
+        columns = {
+          "address",
+          "instructionBytes",
+          "instruction",
+        },
+      }
+    end,
+  },
   {
     "NotAShelf/direnv.nvim",
     config = function()
